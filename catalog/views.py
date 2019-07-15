f59aefe7a84acac003d2253c081a5073a25c9640
Shahed Mehbub <shahed739@gmail.com>
2019-07-16 05:28:20 +0600

    templates, staticfiles and urls added

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+
+def index(request):
+    """View function for homepage of the library site"""
+
+    # Generate count of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a').count()
+
+    # The 'all()' is implied by default
+    num_authors = Author.objects.count()
+
+    num_genres = Genre.objects.all().count()
+    num_books_containing_sky = Book.objects.all().filter(
+        title__contains="sky").count()
+    print(type(num_books_containing_sky))
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_containing_sky': num_books_containing_sky,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
