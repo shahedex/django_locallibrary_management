bb4e006681924870c436c20f449a813e6c51a59c
Shahed Mehbub <shahed739@gmail.com>
2019-07-17 03:19:33 +0600

    added sessions to main page

--- catalog/views.py
+++ catalog/views.py
@@ -17,6 +17,10 @@ def index(request):
     # The 'all()' is implied by default
     num_authors = Author.objects.count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     num_genres = Genre.objects.all().count()
     num_books_containing_sky = Book.objects.all().filter(
         title__contains="sky").count()
@@ -28,6 +32,7 @@ def index(request):
         'num_authors': num_authors,
         'num_genres': num_genres,
         'num_books_containing_sky': num_books_containing_sky,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
