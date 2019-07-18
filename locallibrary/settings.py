950fbbc2c0034d488058f842b40029c3f62df0ae
Shahed Mehbub <shahed739@gmail.com>
2019-07-19 04:53:13 +0600

    Environment variables changed for deployment

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x9+8h5!1l7e3)ss_%96nphjl&rz55onp&6*h**v9yuvauj(ue3'
+#SECRET_KEY = 'x9+8h5!1l7e3)ss_%96nphjl&rz55onp&6*h**v9yuvauj(ue3'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
